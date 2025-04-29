2f216d345f22b1cb6a6b95d0f3a3a834042b9cf1
Siraj Abdulkadir <imsirajabdulkadir@gmail.com>
2025-04-29 04:59:23 -0700

    conflict test;

---
+++ User.java
@@ -0,0 +1,26 @@
+public class User {
+    private String first_name;
+    private String last_name;
+    private int age;
+
+
+    void set_first_name(String first_name){
+      this.first_name=first_name;
+    }
+    void set_last_name(String last_name){
+      this.last_name=last_name;
+    }
+    void set_age(int age){
+      this.age=age;
+    }
+    String get_first_name(){
+      return first_name;
+    }
+    String get_last_name(){
+      return last_name;
+    }
+    int get_age(){
+      return age;
+    }
+    
+}
